f464e7b53fc0761558e194b2d5837525859b1f97
lss555 <chrisheibel7@gmail.com>
2022-06-21 20:40:10 -0600

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Workout from './components/showWorkout/showWorkout.js';
+import Create from './components/createWorkout/createWorkout.js';
+import useStyles from './styles';
+
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+        Workouts Create and Show
+        </Typography>
+
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch">
+              <Grid item xs={12} sm={7}>
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                  <Workout />
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                  <Create />
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
